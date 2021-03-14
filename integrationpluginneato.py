373e991ce4c440e8d1fdb0a8e0117020fa7cfba4
loosrob <79396812+loosrob@users.noreply.github.com>
2021-03-14 19:30:50 +0100

    test maps cont'd

--- integrationpluginneato.py
+++ integrationpluginneato.py
@@ -52,12 +52,13 @@ def setupThing(info):
             thingDescriptors.append(thingDescriptor)
 
         # And let nymea know about all the users robots
+        autoThingsAppeared(thingDescriptors)
+
+        # Get info on available maps on account
         accountMaps = refresh_persistent_maps(account)
         mapsJson = accountMaps.json()
         logger.log("Robot State", rbtStateJson)
-
-        # Get info on available maps on account
-
+        info.thing.availableMaps = accountMaps
 
         # If no poll timer is set up yet, start it now
         logger.log("Creating polltimer")
